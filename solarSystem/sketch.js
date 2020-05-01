4138b788e29935915b088ea45375687499543951
deedeeaar <david.dellarossa@gmail.com>
2020-05-01 05:35:56 +0100

    Exercise complete Latest changes in requirement.

--- solarSystem/sketch.js
+++ solarSystem/sketch.js
@@ -12,19 +12,19 @@ function draw() {
     translate(width/2, height/2); // centre of the sun
         push();
             rotate(radians(speed/3 % 360)); //make the sun spin around its axis
-            celestialObj(color(255,150,0), 300); // SUN
+            celestialObj(color(255,150,0), 200); // SUN
         pop();
         rotate(radians(speed % 360)); //Rotate the earth around the sun
         push(); //start earth coord system
-            translate(0, 200); //centre of the earth
+            translate(0, 300); //centre of the earth
             push();
                 rotate(radians(speed % 360)); //make the earth spin around its axis
-                celestialObj(color(0, 0, 200), 80); //EARTH
+                celestialObj(color(0, 0, 255), 80); //EARTH
             pop();
             rotate(radians(-speed * 2 % 360)) //Rotate the moon around the earth
             push();
-                translate(0, 100); //centre of the moon
-                celestialObj(color(200, 200, 200), 20); //MOON
+                translate(0, -100); //centre of the moon
+                celestialObj(color(255, 255, 255), 30); //MOON
             pop();
         pop();
     pop();
